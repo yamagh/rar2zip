5c251f38dc4aa4bd1aec24c4ee00dafb8f78e959
yamagh <hiroyuki.yamagami@gmail.com>
2015-05-09 15:10:04 +0900

    Support Low/Uppercaser of extension(rar/RAR).

--- rar2zip.sh
+++ rar2zip.sh
@@ -4,13 +4,15 @@ set -eu
 : $1
 
 ext=${1##*.}
-[ $ext != "rar" ] && exit 1
+[ $ext != "rar" -a $ext != "RAR" ] && exit 1
 
 zip_name=`basename $1 '.rar'`.zip
-[ -f $zip_name ] && exit 1
+[ -e $zip_name ] && exit 1
 
 tmp_dir=$(mktemp -d XXXXXX)
 unrar x -inul $1 $tmp_dir
+[ $? -eq 1 ] && exit 1
+
 cd $tmp_dir
 zip -r ../$zip_name *
 cd ..
